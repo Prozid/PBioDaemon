b1b327d6cf9f2c33f167589417607eae4eda364f
Daniel Sanchez <daniprolongo@gmail.com>
2015-03-14 14:18:11 +0100

    Reimplemented results send operation.

--- PBioDaemon/PBioDaemonLauncher/SimulationLauncher.cs
+++ PBioDaemon/PBioDaemonLauncher/SimulationLauncher.cs
@@ -148,58 +148,53 @@ namespace PBioDaemonLauncher
 				sBytes = Encoding.ASCII.GetBytes(errorXML.ToString());
 
 				SendResults(sBytes);
-
 				Estado.Update("Error", idProcess);
 			}            
 		}
 
-
-		private void SendResults(Byte[] sBytes)
+		private void SendResults(Byte[] byteData)
 		{
-			// Preparando conexión
-			IPEndPoint ipep = new IPEndPoint (
-				IPAddress.Parse (config.SERVICE_IP),
-				config.SERVICE_PORT
-			);
-
-			Socket conexion = new Socket (
-				AddressFamily.InterNetwork,
-				SocketType.Stream,
-				ProtocolType.Tcp
-			);
-
-			// Configuramos el envio
-			Console.WriteLine("[" + simulation.IdSimulacion.ToString() + "] Starting to send results...");
-			Byte[] rBytes = new Byte[1024];
-			int raw;           
-
-			// Envio
 			try
 			{
-				// Conectamos con el servicio
-				Console.WriteLine("[" + simulation.IdSimulacion.ToString() + "] Try to connect...");
-				conexion.Connect(ipep);
-				Console.WriteLine("[" + simulation.IdSimulacion.ToString() + "] Connected");
+				// Send data
+				Console.WriteLine("[SEND SIMULATION] Try to connect to " + sConfig.IpDaemon + ":" + sConfig.PortDaemon + "...");
 
-				// Enviamos datos
-				conexion.Send(sBytes);
+				String response_checksum = PBioSocketClient.StartClient(config.SERVICE_IP, config.SERVICE_PORT, byteData);
 
-				// Esperamos confirmación
-				Console.WriteLine("[" + simulation.IdSimulacion.ToString() + "] Waiting answer...");
-				raw = conexion.Receive(rBytes);
+				var sha = new SHA256Managed();
+				byte[] byte_checksum = sha.ComputeHash(byteData);
+				String checksum = BitConverter.ToString(byte_checksum).Replace("-", String.Empty);
 
-
-				Console.WriteLine("[" + simulation.IdSimulacion.ToString() + "] Sended: " + sBytes.LongLength + " Received: " + rBytes.LongLength + "bytes");
-				// Comprobamos confirmación
-				// TODO Comprobar confirmación recepción resultados. Estudiar decisión en caso que veamos que no ha llegado bien.
-
-				// Establecemos a "Completed" la simulación
-				Estado.Update("Completed", idProcess);
+				// Checksum
+				Console.WriteLine("[SEND SIMULATION]Checksum: " + checksum + " Response checksum: " + response_checksum);
+				if (response_checksum != null && response_checksum == checksum)
+				{
+					// Mostramos confirmación
+					Console.WriteLine("[SEND SIMULATION]Confirmation: Sended: TODO show checksum, for example.");
+					// Establecemos a "Completed" la simulación
+					// TODO: Estado completed no existe. Revisar si quitar o qué.
+					//Estado.Update("Completed", idProcess);
+				} else {
+					Console.WriteLine("[SEND SIMULATION] Send failed.");
+					// TODO: Ver que hacer si ha habido un error.
+					Estado.Update("Error", idProcess);
+				}
 			}
-			catch (Exception e)
+			catch (System.TimeoutException error)
 			{
-				Console.WriteLine("[" + simulation.IdSimulacion.ToString() + "] Error: " + e.ToString());
-			}			
+				Console.WriteLine("[SEND SIMULATION] Timeout finished: " + error);
+				Estado.Update("Error", idProcess);
+			}
+			catch (SocketException se)
+			{
+				Console.WriteLine("[SEND SIMULATION] Cannot connect to remote host: " + se.Message);
+				Estado.Update("Error", idProcess);
+			}
+			catch (Exception error)
+			{
+				Console.WriteLine("[SEND SIMULATION] Error desconocido: " + error);
+				Estado.Update("Error", idProcess);
+			}
 		}
 
 		private void proc_Exited(object sender, EventArgs e)
@@ -207,6 +202,155 @@ namespace PBioDaemonLauncher
 			Console.WriteLine("[P] Process finished.");
 		}
 	}
+
+	// State object for receiving data from remote device.
+	public class StateObject
+	{
+		// Client socket.
+		public Socket workSocket = null;
+		// Size of receive buffer.
+		public const int BufferSize = 256;
+		// Receive buffer.
+		public byte[] buffer = new byte[BufferSize];
+		// Received data string.
+		public StringBuilder sb = new StringBuilder();
+	}
+	/// ENVÍO ASÍNCRONO
+	/// http://msdn.microsoft.com/es-es/library/bew39x2a(v=vs.80).aspx
+	class PBioSocketClient
+	{
+		private static System.Diagnostics.EventLog PBioEventLog = PBioEventLogger.initLogger();
+		// ManualResetEvent instances signal completion.
+		private static ManualResetEvent connectDone = new ManualResetEvent(false);
+		private static ManualResetEvent sendDone = new ManualResetEvent(false);
+		private static ManualResetEvent receiveDone = new ManualResetEvent(false);
+		// The response from the remote device.
+		private static String response = String.Empty;
+		public static string StartClient(String ip, int port, byte[] byteData) {
+			try
+			{
+				// Connect to a remote device.
+				// Establish the remote endpoint for the socket.
+				IPAddress ipAddress = IPAddress.Parse(ip);
+				IPEndPoint remoteEP = new IPEndPoint(ipAddress, port);
+				// Create a TCP/IP socket.
+				Socket client = new Socket(AddressFamily.InterNetwork,
+					SocketType.Stream, ProtocolType.Tcp);
+				// Connect to the remote endpoint.
+				client.BeginConnect(remoteEP,
+					new AsyncCallback(ConnectCallback), client);
+				connectDone.WaitOne();
+				// Send test data to the remote device.
+				Send(client, byteData);
+				sendDone.WaitOne();
+				// Receive the response from the remote device.
+				Receive(client);
+				receiveDone.WaitOne();
+				// Write the response to the console.
+				Console.WriteLine("Response received : " + response);
+				// Release the socket.
+				client.Shutdown(SocketShutdown.Both);
+				client.Close();
+			}
+			catch (Exception e)
+			{
+				throw e;
+			}
+			return response;
+		}
+		private static void ConnectCallback(IAsyncResult ar)
+		{
+			try
+			{
+				// Retrieve the socket from the state object.
+				Socket client = (Socket)ar.AsyncState;
+				// Complete the connection.
+				client.EndConnect(ar);
+				Console.WriteLine("Socket connected to " +
+					client.RemoteEndPoint.ToString());
+				// Signal that the connection has been made.
+				connectDone.Set();
+			}
+			catch (Exception e)
+			{
+				throw e;
+			}
+		}
+		private static void Receive(Socket client)
+		{
+			try
+			{
+				// Create the state object.
+				StateObject state = new StateObject();
+				state.workSocket = client;
+				// Begin receiving the data from the remote device.
+				client.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0,
+					new AsyncCallback(ReceiveCallback), state);
+			}
+			catch (Exception e)
+			{
+				throw e;
+			}
+		}
+		private static void ReceiveCallback(IAsyncResult ar)
+		{
+			try
+			{
+				// Retrieve the state object and the client socket
+				// from the asynchronous state object.
+				StateObject state = (StateObject)ar.AsyncState;
+				Socket client = state.workSocket;
+				// Read data from the remote device.
+				int bytesRead = client.EndReceive(ar);
+				if (bytesRead > 0)
+				{
+					// There might be more data, so store the data received so far.
+					state.sb.Append(Encoding.ASCII.GetString(state.buffer, 0, bytesRead));
+					// Get the rest of the data.
+					client.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0,
+						new AsyncCallback(ReceiveCallback), state);
+				}
+				else
+				{
+					// All the data has arrived; put it in response.
+					if (state.sb.Length > 1)
+					{
+						response = state.sb.ToString();
+					}
+					// Signal that all bytes have been received.
+					receiveDone.Set();
+				}
+			}
+			catch (Exception e)
+			{
+				throw e;
+			}
+		}
+		private static void Send(Socket client, byte[] byteData)
+		{
+			// Begin sending the data to the remote device.
+			client.BeginSend(byteData, 0, byteData.Length, 0,
+				new AsyncCallback(SendCallback), client);
+		}
+		private static void SendCallback(IAsyncResult ar)
+		{
+			try
+			{
+				// Retrieve the socket from the state object.
+				Socket client = (Socket)ar.AsyncState;
+				// Complete sending the data to the remote device.
+				int bytesSent = client.EndSend(ar);
+				Console.WriteLine("Sent " +bytesSent + " bytes to server.");
+				// Signal that all bytes have been sent.
+				sendDone.Set();
+			}
+			catch (Exception e)
+			{
+				throw e;
+			}
+		}
+	}
+
 
 }
 
